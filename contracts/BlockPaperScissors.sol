6d0cfadd2f5b021edcf67348f71401e8f1c39626
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-05-25 18:42:25 +0200

    Add functions to encrypt / reveal Move

--- contracts/BlockPaperScissors.sol
+++ contracts/BlockPaperScissors.sol
@@ -21,9 +21,27 @@ contract BlockPaperScissors {
 
     event GameStarted(address firstPlayer, address secondPlayer, bytes32 gameId);
     event FirstMovePlayed(bytes32 gameId, bytes32 encryptedMove);
+    event FirstMoveRevealed(bytes32 gameId, bytes32 encryptedMove, Move decryptedMove, bytes32 secret);
+    event MoveHashCalculated(bytes32 encryptedMove, uint8 decryptedMove, bytes32 secret);
+    event SecondMovePlayed(bytes32 gameId, Move move);
 
-    function startGame(address opponent, bytes32 encryptedMove) public {
-        bytes32 gameId = keccak256(abi.encodePacked(gameCount, msg.sender, opponent));
+    function encryptMove(uint8 move, bytes32 secret) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(move, secret));
+    }
+
+    function decryptMove(bytes32 encryptedMove, bytes32 secret) public pure returns (uint8) {
+        bytes32 currentHash;
+        for(uint8 i = 1; i<4; i++){
+            currentHash = encryptMove(i, secret);
+            if(currentHash == encryptedMove){
+                return(i);
+            }
+        }
+        return 0;
+    }
+
+    function startGame(address opponent, bytes32 encryptedMove) public returns(bytes32 gameId) {
+        gameId = keccak256(abi.encodePacked(gameCount, msg.sender, opponent));
         gameCount++;
 
         games[gameId].state = GameState.Started;
@@ -38,5 +56,27 @@ contract BlockPaperScissors {
         games[gameId].firstMoveEncrypted = encryptedMove;
         emit FirstMovePlayed(gameId, encryptedMove);
     }
+
+    function makeMove(bytes32 gameId, uint8 move) public {
+        require(move <= uint8(Move.Scissors), "Invalid Move: Out of range");
+        require(move > 0, "Invalid Move: Zero");
+        require(msg.sender == games[gameId].secondPlayer, "Sender is not the second player");
+        require(games[gameId].state == GameState.Started, "Can't move in current game state");
+
+        games[gameId].secondMove = Move(move);
+        games[gameId].state = GameState.Played;
+
+        emit SecondMovePlayed(gameId, Move(move));
+    }
+
+    function revealMove(bytes32 gameId, bytes32 secret) public {
+        require(msg.sender == games[gameId].firstPlayer, "Sender is not the first player");
+        require(games[gameId].state == GameState.Played, "Can't reveal move in current game state");
+        bytes32 encryptedMove = games[gameId].firstMoveEncrypted;
+        uint8 decryptedMove = decryptMove(encryptedMove, secret);
+        require(decryptedMove > 0, "No valid move could be decrypted for given secret");
+        emit FirstMoveRevealed(gameId, encryptedMove, Move(decryptedMove), secret);
+    }
+
     
 }
--- test/TestGame.js
+++ test/TestGame.js
@@ -2,22 +2,35 @@ var BlockPaperScissors = artifacts.require("BlockPaperScissors");
 
 contract("TestGame", (accounts) => {
   const owner = accounts[0];
+  const opponent = accounts[1];
 
   describe("BlockPaperScissors", function () {
-    beforeEach(async function () {
+    let gameId;
+    let secret;
+    before(async function () {
       this.contract = await BlockPaperScissors.new({
         from: owner,
       });
     });
 
     it("should be able to start a new Game", async function () {
-      const opponent = accounts[1];
-      console.log(web3.version);
-      console.log(web3.utils.soliditySha3);
-      const secret = web3.utils.soliditySha3('123456');
+      secret = web3.utils.soliditySha3('123456');
       // Hash rock move
-      const hashedMove = web3.utils.soliditySha3(1, secret);
+      const hashedMove = await this.contract.encryptMove.call(1, secret);
+      console.log("encrypted move: ", hashedMove);
+      gameId = await this.contract.startGame.call(opponent, hashedMove);
       await this.contract.startGame(opponent, hashedMove);
+    });
+
+    it("Opponent should be able to make move", async function () {
+      // Paper move
+      const move = 2;
+      await this.contract.makeMove(gameId, move, {from: opponent});
+    });
+
+    it("First Player should be able to reveal move", async function () {
+      // Paper move
+      await this.contract.revealMove(gameId, secret, {from: owner});
     });
   });
 });
