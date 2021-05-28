3376d0fc04e73cf226f9aab2e48fed98bdb66ded
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-05-28 11:06:45 +0200

    Trying to use getContractData but returning not synced errors

--- vapp/src/BlockPaperScissors.vue
+++ vapp/src/BlockPaperScissors.vue
@@ -7,29 +7,60 @@
       :methodArgs="accounts"
     />
 
+    <ul>
+      <li v-for="gameId in gameIds" :key="gameId">{{ gameId }}</li>
+    </ul>
+    <drizzle-contract-form
+      contractName="BlockPaperScissors"
+      method="startGame"
+      :placeholders="placeholders"
+    />
   </div>
 
   <div v-else>Loading...</div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 
 export default {
-  name: 'BlockPaperScissors',
+  name: "BlockPaperScissors",
   computed: {
-    ...mapGetters('accounts', ['activeAccount', 'activeBalance']),
-    ...mapGetters('drizzle', ['isDrizzleInitialized']),
+    ...mapGetters("accounts", ["activeAccount", "activeBalance"]),
+    ...mapGetters("drizzle", ["isDrizzleInitialized", "isDrizzleSynced"]),
+    ...mapGetters("contracts", ["getContractData"]),
 
     accounts() {
-      return [this.activeAccount]
+      return [this.activeAccount];
     },
 
     placeholders() {
-      return ['To Address', 'Amount to Send']
-    }
-  }
-}
+      return ["Opponent Address", "Encrypted Move"];
+    },
+
+    gameIds() {
+      if (this.isDrizzleInitialized) {
+        if (this.isDrizzleSynced) {
+          console.log("Getting Game Ids for Address: ", this.accounts);
+          <!-- const idList = this.getContractData({ -->
+          <!--   contractName: "BlockPaperScissors", -->
+          <!--   method: "getPlayerGames", -->
+          <!--   methodArgs: this.accounts, -->
+          <!-- }); -->
+          if (idList == "loading") {
+            return ["loading"];
+          } else {
+            return idList;
+          }
+        } else {
+          return ["not synced"];
+        }
+      } else {
+        return ["notInitialized"];
+      }
+    },
+  },
+};
 </script>
 
 <style></style>
