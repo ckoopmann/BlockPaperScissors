7b5e755362864948e5ed28d4a8f65ea40c971c5a
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-05-28 18:05:24 +0200

    Load Details for given game in Game Element

--- vapp/src/GameDetails.vue
+++ vapp/src/GameDetails.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="isDrizzleInitialized">
-    <h3>{{ gameId }}</h3>
+    <h3>{{ gameData }}</h3>
   </div>
 
   <div v-else>Loading...</div>
@@ -9,10 +9,64 @@
 <script>
 import { mapGetters } from "vuex";
 
+// Mappings of solidity enum indexes on label
+const states = ["None", "Started", "Played", "Evaluated"]
+const moves = ["None", "Block", "Paper", "Scissors"]
+const results = ["None", "Player 1 Wins", "Player 2 Wins", "Draw"]
+
 export default {
   props: { gameId: { type: String, required: true } },
+  data() {
+    return {
+      contractName: "BlockPaperScissors",
+      method: "getGameData",
+      toUtf8: false,
+      toAscii: false,
+    };
+  },
   computed: {
     ...mapGetters("drizzle", ["isDrizzleInitialized"]),
+    ...mapGetters("contracts", ["getContractData", "contractInstances"]),
+    methodArgs() {
+      return [this.gameId];
+    },
+
+    isStale() {
+      return !this.contractInstances[this.contractName].synced;
+    },
+
+    gameData() {
+      const arg = {
+        contract: this.contractName,
+        method: this.method,
+        toUtf8: this.toUtf8,
+        toAscii: this.toAscii,
+      };
+      let contractData = this.getContractData(arg);
+
+        if(contractData != "loading"){
+            return {
+                state: states[parseInt(contractData["0"])],
+                firstPlayer: contractData["1"],
+                secondPlayer: contractData["2"],
+                firstMoveEncrypted: contractData["3"],
+                firstMoveSecret: contractData["4"],
+                firstMove: moves[parseInt(contractData["5"])],
+                secondMove: moves[parseInt(contractData["6"])],
+                result: results[parseInt(contractData["7"])],
+            }
+        }
+      return contractData;
+    },
+  },
+  created() {
+    const { contractName, method, methodArgs } = this;
+
+    this.$store.dispatch("drizzle/REGISTER_CONTRACT", {
+      contractName,
+      method,
+      methodArgs,
+    });
   },
 };
 </script>
