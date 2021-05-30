b9d5f0b38619c5ad55b9513b3bd226d12d0fabed
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-05-30 14:29:26 +0200

    Clean up contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,5 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
-const TutorialToken = artifacts.require("TutorialToken");
 const BlockPaperScissors = artifacts.require("BlockPaperScissors");
-const ComplexStorage = artifacts.require("ComplexStorage");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(TutorialToken);
   deployer.deploy(BlockPaperScissors);
-  deployer.deploy(ComplexStorage);
 };
--- vapp/src/drizzleOptions.js
+++ vapp/src/drizzleOptions.js
@@ -1,6 +1,3 @@
-import SimpleStorage from './contracts/SimpleStorage.json'
-import ComplexStorage from './contracts/ComplexStorage.json'
-import TutorialToken from './contracts/TutorialToken.json'
 import BlockPaperScissors from './contracts/BlockPaperScissors.json'
 
 const options = {
@@ -11,10 +8,7 @@ const options = {
       url: 'ws://127.0.0.1:9545'
     }
   },
-  contracts: [SimpleStorage, ComplexStorage, TutorialToken, BlockPaperScissors],
-  events: {
-    SimpleStorage: ['StorageSet']
-  },
+  contracts: [BlockPaperScissors],
   polls: {
     accounts: 15000
   }
