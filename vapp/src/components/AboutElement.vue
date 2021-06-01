df5645d9e2066a86f12a6fb2c05ef61a7d322c91
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-06-01 18:35:12 +0200

    Divide About page in sections / elements and put each sections content in a card

---
+++ vapp/src/components/AboutElement.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card class="mx-auto" max-width="800">
+    <v-card-title primary-title class="justify-center">{{
+      title
+    }}</v-card-title>
+    <v-card-text>
+      <v-container class="text-left"><slot></slot></v-container>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: { title: { type: String, required: true } },
+};
+</script>
+
+<style>
+ul {
+  list-style-position: inside;
+}
+</style>
