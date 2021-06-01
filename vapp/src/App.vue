face922412e34f29d234be414d1ca3ed1cc60c38
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-06-01 15:50:46 +0200

    Move Header to its own component

--- vapp/src/App.vue
+++ vapp/src/App.vue
@@ -1,44 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          class="shrink mr-2"
-          contain
-          src="assets/block.png"
-          transition="scale-transition"
-          width="40"
-        />
-        <v-img
-          class="shrink mr-2"
-          contain
-          src="assets/paper.png"
-          transition="scale-transition"
-          width="40"
-        />
-        <v-img
-          class="shrink mr-2"
-          contain
-          src="assets/scissors.png"
-          transition="scale-transition"
-          width="40"
-        />
-      </div>
-
-      <v-app-bar-title>Block Paper Scissors</v-app-bar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/ckoopmann/BlockPaperScissors"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Github Repo</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <TheHeader />
     <v-main>
       <div class="section">
         <router-view />
@@ -49,9 +11,11 @@
 
 <script>
 import { mapGetters } from "vuex";
+import TheHeader from "./components/TheHeader.vue";
 
 export default {
   name: "app",
+  components: { TheHeader },
 
   computed: mapGetters("drizzle", ["isDrizzleInitialized"]),
 };
---
+++ vapp/src/components/TheHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">
+      <v-img
+        class="shrink mr-2"
+        contain
+        src="assets/block.png"
+        transition="scale-transition"
+        width="40"
+      />
+      <v-img
+        class="shrink mr-2"
+        contain
+        src="assets/paper.png"
+        transition="scale-transition"
+        width="40"
+      />
+      <v-img
+        class="shrink mr-2"
+        contain
+        src="assets/scissors.png"
+        transition="scale-transition"
+        width="40"
+      />
+    </div>
+
+    <v-app-bar-title>Block Paper Scissors</v-app-bar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn to="games" text>
+      <span class="mr-2">Your Games</span>
+    </v-btn>
+
+    <v-btn
+      href="https://github.com/ckoopmann/BlockPaperScissors"
+      target="_blank"
+      text
+    >
+      <span class="mr-2">Github Repo</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
