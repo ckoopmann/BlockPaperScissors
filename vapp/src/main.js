015fb4937326dc4166f7ceda63be39ee674f2b9d
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-06-03 10:49:23 +0200

    Remove drizzle options file

--- vapp/src/main.js
+++ vapp/src/main.js
@@ -1,17 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-// Todo: Update this when publishing
-// import drizzleVuePlugin from '@drizzle/vue-plugin'
-//
-import drizzleVuePlugin from '@drizzle/vue-plugin'
-import drizzleOptions from './drizzleOptions'
 import vuetify from './plugins/vuetify'
 import router from './router'
 import store from './store'
 
 
-Vue.use(drizzleVuePlugin, { store, drizzleOptions })
 
 Vue.config.productionTip = false
 
