ccd463da496bcded2238c8e9b796925ecd6b00e5
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-06-03 19:19:45 +0200

    Improve About page

--- vapp/src/components/AboutElement.vue
+++ vapp/src/components/AboutElement.vue
@@ -1,16 +1,14 @@
 <template>
-  <v-carousel-item :src="src" height="1000">
-    <v-row class="fill-height" align="center" justify="center">
+    <v-row>
       <v-card class="mx-auto" max-width="800">
         <v-card-title primary-title class="justify-center">{{
           title
         }}</v-card-title>
         <v-card-text>
-          <v-container class="text-left"><slot></slot></v-container>
+          <v-container class="text-left text-body-1"><slot></slot></v-container>
         </v-card-text>
       </v-card>
     </v-row>
-  </v-carousel-item>
 </template>
 
 <script>
--- vapp/src/views/About.vue
+++ vapp/src/views/About.vue
@@ -1,76 +1,49 @@
 <template>
-  <div class="about" style="height:100%">
-    <v-carousel :continuous="false" style="height:90%">
-      <h1 class="mt-4 mb-4 white--text">Block Paper Scissors</h1>
-      <h4 class="mb-4 white--text">Your favourite game - on chain</h4>
-      <AboutElement
-        title="How it all began"
-        class="mb-4"
+  <div class="about">
+    <h1 class="mt-4 mb-4 white--text">Block Paper Scissors</h1>
+    <h4 class="mb-4 white--text">Your favourite game - on chain</h4>
+    <AboutElement title="What is Block Paper Scissors ?" class="mb-4">
+      This is the decentralized, censorship-resistant and permissionless version
+      of the classic game
+      <a href="https://en.wikipedia.org/wiki/Rock_paper_scissors">
+        Rock Paper Scissors</a
       >
-        You have probably played it many times before, a game as simple as it
-        gets.
-        <a href="https://en.wikipedia.org/wiki/Rock_paper_scissors">
-          Rock Paper Scissors</a
-        >: You and your opponent both secretly choose one of three allowed moves
-        listed in the name of the game. Then both of you reveal your choice and
-        the game is evaluated according to the following rules:
-        <ul>
-          <li>Rock beats Scissors</li>
-          <li>Paper beats Scissors</li>
-          <li>Scissors beat Paper</li>
-          <li>If both players choose the same move the game ends in a draw</li>
-        </ul>
-      </AboutElement>
+      running on the Ethereum blockchain.
+      <br />
+      <br />
 
-      <AboutElement
-        title="Enter the Blockchain"
-        class="mb-4"
-      >
-        Thanks to Block Paper Scissors you can now play this game via a smart
-        contract on the ethereum block chain. This gives you a variety of
-        advantages over the traditional "implementation":
-        <ul>
-          <li>Play with anyone around the world</li>
-          <li>
-            Game evaluation is done in a trustless / decentralized / transparent
-            way
-          </li>
-          <li>Game results are recorded on-chain for eternity</li>
-          <li>
-            Contract can be integrated into other contracts / systems to settle
-            "Coin-Flip" decisions between two participants without the need for
-            (pseudo-)random number generation
-          </li>
-        </ul>
-      </AboutElement>
+      All you need to play is your
+      <a href="https://metamask.io/"> Metamask</a> wallet and off you go.
+      <br />
+      <br />
 
-      <AboutElement title="How it works" class="mb-4">
-        The core logic of the game is implemented in a solidity smart-contract
-        that includes the following steps:
-        <ol>
-          <li>Player 1 decides on a move and chooses a secret string</li>
-          <li>
-            Using the chosen secret the move is encrypted in the client dapp
-          </li>
-          <li>
-            Player 1 calls the smart contract to create the game providing his
-            encrypted move and the ethereum adress of Player 2
-          </li>
-          <li>
-            Player 2 calls the smart contract to provide his move (without
-            encryption)
-          </li>
-          <li>
-            Player 1 calls the smart contract to evaluate the game by providing
-            the secret with which he encrypted his move previously
-          </li>
-          <li>
-            Inside the smart contract Player 1's move is decrypted and used
-            together with the second move to evaluate the game.
-          </li>
-        </ol>
-      </AboutElement>
-    </v-carousel>
+      The code for this dapp including the smart contract that implements the
+      game logic can be found in the
+      <a href="https://github.com/ckoopmann/BlockPaperScissors"> Github-Repo</a
+      >.
+    </AboutElement>
+
+    <AboutElement title="How do I play ?" class="mb-4">
+      A round of Block Paper Scissors is played in 3 steps, each corresponding
+      to one ethereum transaction.
+      <ol>
+        <li>
+          Player 1 creates a New Game providing the opponents address and his
+          own "Move" encrypted with a secret key
+        </li>
+        <li>
+          Player 2 submits his move.
+        </li>
+        <li>
+          Player 1 reveals his move by providing the secret key he used in Step
+          1 and the game is evaluated
+        </li>
+      </ol>
+      A new game can be opened using the button at the top of the page while all ongoing / evaluated games
+      can be viewed here <router-link to="/games">here</router-link>, including games to which other players have
+      challenged you.
+    </AboutElement>
+
   </div>
 </template>
 
@@ -87,17 +60,16 @@ export default {
 ul {
   list-style-position: inside;
 }
-.v-image__image--cover {
-  width: 100%;
-  overflow: hidden;
-  display: flex;
-  justify-content: center;
-  object-fit: cover !important;
+ol li {
+  padding: 5px;
+  margin-left: 35px;
 }
 .about {
   background-image: url(/assets/hands.jpg);
   background-size: cover; /* <------ */
   background-repeat: no-repeat;
   background-position: center center;
+  background-attachment: fixed;
+  display: inline-table;
 }
 </style>
