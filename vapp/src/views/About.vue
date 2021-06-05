72ae9f041aef209debf155a85663c782ed4cf699
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-06-05 10:52:59 +0200

    Extend the about page

--- vapp/src/views/About.vue
+++ vapp/src/views/About.vue
@@ -11,21 +11,31 @@
       <br />
       <br />
 
-      All you need to play is your
-      <a href="https://metamask.io/"> Metamask</a> wallet and off you go.
+      This is a web3 application therefore you will need to install / connect
+      the <a href="https://metamask.io/"> Metamask</a> smart wallet to use its
+      features.
       <br />
       <br />
 
       The code for this dapp including the smart contract that implements the
       game logic can be found in the
       <a href="https://github.com/ckoopmann/BlockPaperScissors"> Github-Repo</a
-      >.
+      >. The link to this repository as well as to any other part of the
+      application is accesible via the menu .
 
       <br />
       <br />
-      Note that the smart contract underlying this dapp is currently only <b>deployed on Rinkeby</b> test-net
-      therefore you have to switch the network of your wallet accordingly to try it out.
+      Note that the smart contract underlying this dapp is currently only
+      <b>deployed on Rinkeby</b> test-net therefore you have to switch the
+      network of your wallet accordingly to try it out.
 
+      <br />
+      <br />
+      Also note that since all actions in this application (such as creating a
+      new game), are executed via ethereum transactions they will take more time
+      than you might be used to from a centralized application. The app is set
+      up to listen to all relevant contract events such that the data should be
+      updated automatically without you having to refresh the page.
     </AboutElement>
 
     <AboutElement title="How do I play ?" class="mb-4">
@@ -44,11 +54,31 @@
           1 and the game is evaluated
         </li>
       </ol>
-      A new game can be opened using the button at the top of the page while all ongoing / evaluated games
-      can be viewed <router-link to="/games">here</router-link>, including games to which other players have
-      challenged you.
+      A new game can be opened using the button at the top of the page while all
+      ongoing / evaluated games can be viewed
+      <router-link to="/games">here</router-link>, including games to which
+      other players have challenged you.
     </AboutElement>
 
+    <AboutElement title="What are the rules ?" class="mb-4">
+      In case you are not aware of the original Rock Paper Scissors game here
+      are the rules that are used to evaluate a game result once both players
+      have submitted / revealed their moves:
+      <ol>
+        <li>
+          If both moves are identical the game ends in a draw
+        </li>
+        <li>
+          Paper beats Block
+        </li>
+        <li>
+          Scissors beat Paper
+        </li>
+        <li>
+          Block beats Scissors
+        </li>
+      </ol>
+    </AboutElement>
   </div>
 </template>
 
