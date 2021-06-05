933ec42858e0cf020bc51aab3195a0fba9dc92d3
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-06-05 11:32:52 +0200

    Update opponent address while typing and only fire transaction when opponent address is valid

--- vapp/src/components/dialogues/NewGame.vue
+++ vapp/src/components/dialogues/NewGame.vue
@@ -10,7 +10,7 @@
         <v-card-title>
           <span class="headline">New Game </span>
         </v-card-title>
-        <form @submit.prevent="submit">
+        <form @submit.prevent="submit" ref="newGameForm">
           <v-card-text>
             <v-container>
               <v-row>
@@ -44,7 +44,8 @@
                   <v-combobox
                     label="Opponent Address"
                     hint="Address of the account who you want to challenge for a game"
-                    v-model="opponent"
+                    id="opponent"
+                    @update:search-input="updateOpponent"
                     :rules="[validAddress, notNullAddress, distinctAddress]"
                     :items="previousOpponents"
                     required
@@ -89,11 +90,20 @@ export default {
 
   methods: {
     ...mapActions("contractModule", ["startGame"]),
+    // Adding this to force continously updating the opponent address as the user types (and not just on blure as is default for v-combobox)
+    // See https://github.com/vuetifyjs/vuetify/issues/4679#issuecomment-682456398
+    updateOpponent(currentValue) {
+      this.opponent = currentValue;
+    },
     async submit(event) {
       const { title, opponent, secret, move } = this;
-      await this.startGame({ title, opponent, secret, move });
-      this.dialog = false;
-      this.$router.push("/games");
+      if (this.validateOpponent(opponent)) {
+        await this.startGame({ title, opponent, secret, move });
+        this.dialog = false;
+        this.$router.push("/games");
+      } else {
+        console.log("Validation Failed");
+      }
     },
     close() {
       this.dialog = false;
@@ -112,6 +122,13 @@ export default {
     },
     distinctAddress(address) {
       return address != this.activeAccount || "Cannot play against yourself";
+    },
+    validateOpponent(address) {
+      return (
+        this.validAddress(address) === true &&
+        this.notNullAddress(address) === true &&
+        this.distinctAddress(address) === true
+      );
     },
   },
 };
